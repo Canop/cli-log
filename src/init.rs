668b53a7aad72fd37621d21165c4995751d3424c
Canop <cano.petrole@gmail.com>
2023-10-13 17:35:56 +0200

    remove useless references

--- src/init.rs
+++ src/init.rs
@@ -20,13 +20,13 @@ pub fn init(app_name: &str, app_version: &str) {
         "{}_LOG",
         app_name.to_ascii_uppercase().replace('-', "_"),
     );
-    let level = env::var(&env_var_name).unwrap_or_else(|_| "off".to_string());
+    let level = env::var(env_var_name).unwrap_or_else(|_| "off".to_string());
     if level == "off" {
         return;
     }
     if let Ok(level) = LevelFilter::from_str(&level) {
         let log_file_name = format!("{}.log", app_name);
-        let file = File::create(&log_file_name)
+        let file = File::create(log_file_name)
             .expect("Log file can't be created");
         log::set_max_level(level);
         let logger = FileLogger {
